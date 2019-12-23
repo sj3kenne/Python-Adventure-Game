0bdffd47bda994121d8a7f119261b57059a7de42
sj3kenne <32079919+sj3kenne@users.noreply.github.com>
2019-12-22 22:41:34 -0500

    Update tiles.py

--- tiles.py
+++ tiles.py
@@ -5,10 +5,42 @@ class MapTile:
         self.x = x
         self.y = y
 
-def intro_text(self):
-    raise NotImplementedError()
+    def intro_text(self):
+        raise NotImplementedError()
 
-def modify_player(self, player):
-    raise NotImplementedError()
+    def modify_player(self, player):
+        raise NotImplementedError()
+
+class StartingRoom(MapTile):
+    def intro_text(self):
+        return """
+        You find yourself in a cave with a flickering torch on the wall. You can make out four paths, equally as dark and foreboding.
+        """
+
+    def modify_player(self, player):
+        #Room has no action on player
+        pass
+
+class LootRoom(MapTile):
+    def __init__(self, x, y, item):
+        self.item = item.super().__init__(x, y)
+        super().__init__(x, y)
+
+    def add_loot(self, player):
+        player.inventory.append(self.item)
+
+    def modify_player(self, player):
+        self.add_loot(player)
+
+class EnemyRoom(MapTile):
+    def __init__(self, x, y, enemy):
+        self.enemy = enemy
+        super().__init__(x, y)
+
+    def modify_player(self, the_player):
+        if self.enemy.is_alive():
+            the_player.hp = the_player.hp - self.enemy.damage
+            print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
+
 
 
