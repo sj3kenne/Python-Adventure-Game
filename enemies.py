bb1396529a9f4f18793a20e8f26bdf38a96295f3
sj3kenne <32079919+sj3kenne@users.noreply.github.com>
2019-12-22 21:40:43 -0500

    step and a bit

---
+++ enemies.py
@@ -0,0 +1,22 @@
+"""Enemies"""
+class Enemy:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+
+    def is_alive(self):
+        return self.hp > 0
+
+class GiantSpider(Enemy):
+    def __init__(self):
+        super().__init__(name="Giant Spider", hp=10, damage=2)
+
+class Orge(Enemy):
+    def __init__(self):
+        super().__init__(name="Orge", hp=30, damage=15)
+
+        
+
+
+
---
+++ tiles.py
@@ -0,0 +1,14 @@
+import items, enemies
+
+class MapTile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def intro_text(self):
+    raise NotImplementedError()
+
+def modify_player(self, player):
+    raise NotImplementedError()
+
+
