46c319d34466e1412b6dc69a61b8ed0599858043
sj3kenne <32079919+sj3kenne@users.noreply.github.com>
2020-03-04 16:29:36 -0500

    sync

--- tiles.py
+++ tiles.py
@@ -1,4 +1,4 @@
-import items, enemies
+import items, enemies, actions, world
 
 class MapTile:
     def __init__(self, x, y):
@@ -10,6 +10,26 @@ class MapTile:
 
     def modify_player(self, player):
         raise NotImplementedError()
+
+    def adjacent_moves(self):
+        """Returns all move actions for adjacent tiles."""
+        moves = []
+        if world.tile_exists(self.x + 1, self.y):
+            moves.append(actions.MoveEast())
+        if world.tile_exists(self.x - 1, self.y):
+            moves.append(actions.MoveWest())
+        if world.tile_exists(self.x, self.y - 1):
+            moves.append(actions.MoveNorth())
+        if world.tile_exists(self.x, self.y + 1):
+            moves.append(actions.MoveSouth())
+        return moves
+ 
+    def available_actions(self):
+        """Returns all of the available actions in this room."""
+        moves = self.adjacent_moves()
+        moves.append(actions.ViewInventory())
+ 
+        return moves
 
 class StartingRoom(MapTile):
     def intro_text(self):
@@ -41,6 +61,12 @@ class EnemyRoom(MapTile):
         if self.enemy.is_alive():
             the_player.hp = the_player.hp - self.enemy.damage
             print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
+
+    def available_actions(self):
+        if self.enemy.is_alive():
+            return [actions.Flee(tile=self), actions.Attack(enemy=self.enemy)]
+        else:
+            return self.adjacent_moves()    
 
 class EmptyCavePath(MapTile):
     def intro_text(self):
@@ -76,3 +102,25 @@ class FindDaggerRoom(LootRoom):
         It's a dagger! You pick it up.
         """
 
+class Find5GoldRoom(LootRoom):
+    def __init__(self, x, y):
+        super(Find5GoldRoom, self).__init__(x, y, items.Gold(5))
+
+    def intro_text(self):
+        return """
+        You find a few coins scattered around the room.
+        """
+
+
+class LeaveCaveRoom(MapTile):
+    def intro_text(self):
+        return """
+        You see a bright light in the distance...
+        ... it grows as you get closer! It's sunlight!
+
+        Victory is yours!
+        """
+    def modify_player(self, player):
+        player.victory = True
+
+    
